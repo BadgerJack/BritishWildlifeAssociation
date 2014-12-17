70c07f5ebdf1a3871633b91726bf365c35386058
hobbes <hobbes@localhost>
2014-12-17 13:05:09 +0000

    Edited Observer comments

--- src/britishwildlifeassociation/Observer.java
+++ src/britishwildlifeassociation/Observer.java
@@ -3,6 +3,10 @@ package britishwildlifeassociation;
 /**
  *
  * @author hobbes
+ *
+ * This interface left intentionally blank
+ * Used to reference child classes of the interface
+ * 
  */
 public interface Observer {
 }
