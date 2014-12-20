9cf04736ac4625cebb029552774d26ae5f7ab8d9
hobbes <hobbes@localhost>
2014-12-20 16:31:23 +0000

    Moved model lists to MVC

--- src/britishwildlifeassociation/ModelViewController.java
+++ src/britishwildlifeassociation/ModelViewController.java
@@ -1,5 +1,7 @@
 package britishwildlifeassociation;
 
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author hobbes
@@ -11,6 +13,34 @@ package britishwildlifeassociation;
  */
 public class ModelViewController {
 
+    //Lists used by boxes in GUI
+    static DefaultListModel animList = new DefaultListModel();
+    static DefaultListModel profList = new DefaultListModel();
+    static DefaultListModel volList = new DefaultListModel();
+    static DefaultListModel obsList = new DefaultListModel();
+    
+    //Prepares above lists with values from arraylists
+    public void populate()
+    {
+        for (int i = 0; i < BritishWildlifeAssociation.animals.size(); ++i) {
+            animList.addElement(BritishWildlifeAssociation.animals.get(i).getName());
+        }
+        for (int i = 0; i < BritishWildlifeAssociation.professionals.size(); ++i) {
+            profList.addElement(BritishWildlifeAssociation.professionals.get(i).getPrevStatus().getFirstName() + " "
+                    + BritishWildlifeAssociation.professionals.get(i).getPrevStatus().getLastName() + " "
+                    + BritishWildlifeAssociation.professionals.get(i).getPrevStatus().getAddress()
+            );
+        }
+        for (int i = 0; i < BritishWildlifeAssociation.volunteers.size(); ++i) {
+            volList.addElement(BritishWildlifeAssociation.volunteers.get(i).getFirstName() + " "
+                    + BritishWildlifeAssociation.volunteers.get(i).getLastName() + " "
+                    + BritishWildlifeAssociation.volunteers.get(i).getAddress());
+        }
+        for (int i = 0; i < BritishWildlifeAssociation.observations.size(); ++i) {
+            obsList.addElement(BritishWildlifeAssociation.observations.get(i).getAnimal().getName() + " "
+                    + BritishWildlifeAssociation.observations.get(i).getAnimal().getThreatLevel());
+        }
+    }
     //Functions for controlling Animal objects
     static void createAnimal(String newName, String newThreatLevel) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
